7be57af7a9296e3176fd5db52c2589cafcbdc501
lnu <l.nullens@gmail.com>
2018-04-11 09:09:22 +0200

    GitVersion marked as dev dependency (PrivateAssets="All")

--- NHibernate.Logging.Serilog/SerilogLoggerFactory.cs
+++ NHibernate.Logging.Serilog/SerilogLoggerFactory.cs
@@ -1,23 +1,23 @@
 ﻿using Serilog;
+using Serilog.Core;
 
 namespace NHibernate.Logging.Serilog
 {
-    /// <summary>
-    /// Factory to create SerilogLogger
-    /// </summary>
-    public class SerilogLoggerFactory : INHibernateLoggerFactory
-    {
+	/// <summary>
+	/// Factory to create SerilogLogger
+	/// </summary>
+	public class SerilogLoggerFactory : INHibernateLoggerFactory
+	{
+		public INHibernateLogger LoggerFor(string keyName)
+		{
+			var contextLogger = Log.Logger.ForContext(Constants.SourceContextPropertyName, keyName);
+			return new SerilogLogger(contextLogger);
+		}
 
-        public INHibernateLogger LoggerFor(string keyName)
-        {
-            var contextLogger = Log.Logger.ForContext("SourceContext", keyName);
-            return new SerilogLogger(contextLogger);
-        }
-
-        public INHibernateLogger LoggerFor(System.Type type)
-        {
-            var contextLogger = Log.Logger.ForContext(type);
-            return new SerilogLogger(contextLogger);
-        }
-    }
+		public INHibernateLogger LoggerFor(System.Type type)
+		{
+			var contextLogger = Log.Logger.ForContext(type);
+			return new SerilogLogger(contextLogger);
+		}
+	}
 }
